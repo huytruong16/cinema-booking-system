6737602609f6923c4be4dab4661197ad97d0ccaa
Nguyen Ba Tuan Anh <nguyenbatuananh2k5@gmail.com>
2025-10-21 15:13:24 +0700

    feat: config prisma module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
-import { AuthController } from './modules/auth/auth.controller';
-import { AuthService } from './modules/auth/auth.service';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [UsersModule, AuthModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService, AuthService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
