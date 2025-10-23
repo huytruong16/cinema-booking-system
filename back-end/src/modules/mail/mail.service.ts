e8d8b14f371cab55dee9472c5c7e6e13a2fcf13a
huytruong16 <huytd241@gmail.com>
2025-10-23 15:55:42 +0700

    feat: add email template for sending OTP

--- back-end/src/modules/mail/mail.service.ts
+++ back-end/src/modules/mail/mail.service.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { readFileSync } from 'fs';
 import nodemailer from 'nodemailer';
+import { join } from 'path';
+
+const emailTemplates = {
+    verify_email: {
+        email_title: "Xác minh email",
+        email_heading: "Xác minh email",
+        otp_purpose: "Mã OTP để xác minh email của bạn là",
+        instruction_text: "Mã OTP này dùng để xác nhận email khi đăng ký tài khoản mới."
+    },
+    reset_password: {
+        email_title: "Thay đổi mật khẩu",
+        email_heading: "Thay đổi mật khẩu",
+        otp_purpose: "Mã OTP để xác minh tài khoản của bạn là",
+        instruction_text: "Mã OTP này dùng để thay đổi mật khẩu mới."
+    }
+};
 
 @Injectable()
 export class MailService {
@@ -19,13 +36,53 @@ export class MailService {
         });
     }
 
-    async sendMail(to: string, subject: string, text: string) {
+    async sendOTPEmail(
+        to: string,
+        subject: string,
+        otp: string,
+        fullName: string,
+        emailType: 'register' | 'forgot-password'
+    ) {
         const smtpConfig = this.configService.get('smtp');
+
+        const emailConfig = {
+            'register': {
+                template: 'email-verify-otp.hbs',
+                title: 'Xác minh email',
+                heading: 'Xác minh email',
+                purpose: 'Mã OTP để xác minh tài khoản của bạn là',
+                instruction: 'Mã OTP này dùng để xác nhận email khi đăng ký tài khoản mới.'
+            },
+            'forgot-password': {
+                template: 'email-forgot-password-otp.hbs',
+                title: 'Thay đổi mật khẩu',
+                heading: 'Thay đổi mật khẩu',
+                purpose: 'Mã OTP để xác minh tài khoản của bạn là',
+                instruction: 'Mã OTP này dùng để thay đổi mật khẩu mới.'
+            }
+        };
+
+        const config = emailConfig[emailType];
+
+        const htmlTemplate = readFileSync(
+            join(__dirname, '..', '..', '..', 'templates', 'email-otp.hbs'),
+            'utf-8'
+        );
+
+        let htmlContent = htmlTemplate
+            .replace('{{otp}}', otp)
+            .replace('{{user_name}}', fullName)
+            .replace('{{email_title}}', config.title)
+            .replace('{{email_heading}}', config.heading)
+            .replace('{{otp_purpose}}', config.purpose)
+            .replace('{{instruction_text}}', config.instruction);
+
         await this.transporter.sendMail({
             from: `"Support" <${smtpConfig.from}>`,
             to,
             subject,
-            text,
+            html: htmlContent
         });
     }
+
 }
