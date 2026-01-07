0a55e8e90936dd96622c65c82ffc1cccca56cde9
huytruong16 <huytd241@gmail.com>
2026-01-07 21:56:00 +0700

    feat:  update invoice email template with movie poster and QR code

--- back-end/src/modules/invoice/invoice.module.ts
+++ back-end/src/modules/invoice/invoice.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { PayosService } from 'src/libs/common/services/payos.service';
 import { TicketsService } from '../tickets/tickets.service';
 import { PdfService } from '../pdf/pdf.service';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, MailModule],
   controllers: [InvoiceController],
   providers: [InvoiceService, PayosService, TicketsService, PdfService],
   exports: [InvoiceService],
